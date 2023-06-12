ceed55f1ccd43b5fab3757b27ef03fd36413e01b
juliannesay <jmsay@wisc.edu>
2023-06-12 19:32:29 -0400

    completion of activity 3

--- Chapter02/js/main_with_debug.js
+++ Chapter02/js/main_with_debug.js
@@ -1,74 +1,60 @@
-var cityPop = [
-	{ 
-		city: 'Madison',
-		population: 233209
-	},
-	{
-		city: 'Milwaukee',
-		population: 594833
-	},
-	{
-		city: 'Green Bay',
-		population: 104057
-	},
-	{
-		city: 'Superior',
-		population: 27244
-	}
-];
-
-function addColumns(cityPop){
-    
-    document.querySelectorAll("tr").forEach(function(row, i){
-
-    	if (i == 0){
-
-    		row.insertAdjacntHTML('beforeend', '<th>City Size</th>');
-    	} else {
-
-    		var citySize;
-
-    		if (cityPop[i-1].population < 100000){
-    			citySize = 'Small';
-
-    		} else if (cityPop[i-1].population < 500000){
-    			citysize = 'Medium';
-
-    		} else {
-    			citySize = 'Large';
-    		};
-
-			row.insertAdjacntHTML = '<td' + citySize + '</td>';
-    	};
-    });
+//initialize function called when script loads
+function initialize(){
+    cities();
 };
 
-function addEvents(){
+//function to create a table with cities and their populations
+function cities(){
+    var cities = [
+        'Madison',
+        'Milwaukee',
+        'Green Bay',
+        'Superior'
+    ];
+    var population = [
+        233209,
+        594833,
+        104057,
+        27244
+    ];
 
-	document.querySelector("table").addEventListener("mouseover", function(){
-		
-		var color = "rgb(";
+    //create a table element
+    var table = document.createElement("table");
 
-		for (var i=0; i<3; i++){
+    //create a header row
+    var headerRow = document.createElement("tr");
 
-			var random = Math.round(Math.random() * 255);
+    //add city column to header row
+    var cityHeader = document.createElement("th");
+    cityHeader.innerHTML = "City";
+    headerRow.appendChild(cityHeader);
 
-			color += "random";
+    //add population column to header row
+    var popHeader = document.createElement("th");
+    popHeader.innerHTML = "Population";
+    headerRow.appendChild(popHeader);
 
-			if (i<2){
-				color += ",";
-			
-			} else {
-				color += ")";
-		};
+    //add the header row
+    table.appendChild(headerRow);
 
-		document.querySelector("table").color = color;
-	});
+    //loop to add a new row for each city
+    for (var i = 0; i < cities.length; i++){
+        var tr = document.createElement("tr");
 
-	function clickme(){
+        var city = document.createElement("td");
+        city.innerHTML = cities[i];
+        tr.appendChild(city);
 
-		alert('Hey, you clicked me!');
-	};
+        var pop = document.createElement("td");
+        pop.innerHTML = population[i];
+        tr.appendChild(pop);
 
-	document.querySelector("table").addEventListener("click", clickme)
+        table.appendChild(tr);
+    };
+
+    //add the table to the div in index.html
+    var mydiv = document.getElementById("mydiv");
+    mydiv.appendChild(table);
 };
+
+window.onload = initialize();
---
+++ Chapter03/js/main.js
@@ -0,0 +1,26 @@
+// Add all scripts to the JS folder
+function jsAjax(){
+    // Step 1: Create the data request 
+    var request = new Request('data/MegaCities.geojson');
+    //Step 2: define Fetch parameters 
+    var init = {
+        method: 'GET'
+    }
+    //Step 3: use Fetch to retrieve the data
+    .then(conversion) //Step 4 convert data to usable form
+    .then(callback) //Step 5 Send retrieved data to a callback function
+};
+
+//define conversion callback function
+function conversion(response){
+    //convert data to usable form
+    return response.json();
+  }
+  
+  //define callback function
+  function callback(response){
+      //tasks using the data go here
+      console.log(JSON.stringify(response));
+  }
+
+window.onload = jsAjax();
